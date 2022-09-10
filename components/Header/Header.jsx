7ee50d28b47f891282bd3cb0c81fea9409bc8b57
Azamat CTO <azamatcta@gmail.com>
2022-09-10 21:53:26 +0500

    Initial commit

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,5 +1,6 @@
-import { AppBar, Box, IconButton, Toolbar } from '@mui/material'
+import {AppBar, Box, IconButton, Menu, MenuItem, Toolbar} from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
+import AccountIcon from '@mui/icons-material/AccountCircle'
 
 import Logo from './Logo'
 import { useState } from 'react'
@@ -8,6 +9,8 @@ import Navbar from './Navbar'
 
 const Header = () => {
   const [drawer, setDrawer] = useState(false)
+  const [auth, setAuth] = useState(true)
+  const [anchorEl, setAnchorEl] = useState(null)
 
   const handleDrawerOpen = () => {
     setDrawer(true)
@@ -17,20 +20,68 @@ const Header = () => {
     setDrawer(false)
   }
 
+  const handleChange = (event) => {
+    setAuth(event.target.checked)
+  }
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
+
   return (
     <>
       <Box>
         <AppBar position={'static'}>
           <Toolbar>
             <Logo />
+
             <Box flexGrow={1}></Box>
+
             <Navbar />
+
+            {auth && (
+              <div>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  color="inherit"
+                  onClick={handleMenu}
+                >
+                  <AccountIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right'
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right'
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={handleClose}>My account</MenuItem>
+                </Menu>
+              </div>
+            )}
+
             <IconButton
               size={'large'}
               edge={'start'}
               color={'inherit'}
               aria-label={'open drawer'}
-              sx={{ ml: 2, display: { md: 'none' } }}
+              sx={{ ml: 1, display: { md: 'none' } }}
               onClick={handleDrawerOpen}
             >
               <MenuIcon />
